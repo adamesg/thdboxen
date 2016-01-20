1e115618c0f3236a4a05d7fea35e1a77b98d20be
ComDev <comdev@ComDevs-MacBook-Pro.local>
2016-01-20 16:50:30 -0500

    initial commit

--- manifests/site.pp
+++ manifests/site.pp
@@ -88,3 +88,79 @@ node default {
     target => $boxen::config::repodir
   }
 }
+
+  ## Install CloudFoundry
+  ## brew tap pivotal/tap
+  homebrew::tap { 'pivotal/tap': }
+  
+  ## brew install cloudfoundry-cli
+  package { "pivotal/tap/cloudfoundry-cli":
+    ensure => present,
+    require => Homebrew::Tap['pivotal/tap'],
+  } ## -> installs `cloudfoundry-cli` to `/opt/boxen/homebrew/Cellar/cloudfoundry-cli/`
+
+ include iterm2::stable 
+ include slack
+
+class slack (
+  $version = '1.1.10'
+) {
+  # Application in a .zip
+  package { 'Slack':
+    ensure   => present,
+    source   => "https://slack-ssb-updates.global.ssl.fastly.net/mac_public_releases/slack-${version}.zip",
+    provider => compressed_app
+  }
+}
+
+  package { "IntelliJ-IDEA-IU-15.0.3":
+    provider => 'appdmg_eula',
+    source   => "http://download.jetbrains.com/idea/ideaIU-15.0.3.dmg",
+  }
+ 
+
+include brewcask
+package { 'java': provider => 'brewcask' } 
+
+  package { 'CiscoAnyConnect':
+    source => '/Users/comdev/Desktop/corpdeploy/Install and-or add to Dock/anyconnect-macosx-i386-3.1.12020-k9-2.dmg',
+    provider => pkgdmg,
+  }
+
+  package {'SEP' :
+    source => '/Users/comdev/Desktop/corpdeploy/SEP 12.1 R6 MP2/SEP 12.1 R6 MP2 Application Installer/Additional Resources/SEP.mpkg',
+    provider => apple,
+  }
+
+  package { 'Outlook':
+    source => '/Users/comdev/Desktop/Microsoft_Office_2016_Volume_Installer.pkg',
+    provider => apple,
+  }
+
+  package { 'AirWatch':
+    source => '/Users/comdev/Downloads/AirWatchAgent.dmg',
+    provider => pkgdmg,
+  }
+
+  file { 'HD-Firewall':
+    source => '/Users/comdev/Desktop/corpdeploy/Install and-or add to Dock/HD-Firewall.term',
+    path => '/Applications/HD-Firewall.term',
+  }
+
+
+
+#  class associate-user {
+#    include 'stdlib'
+#    $pass = str2saltedsha512('password')
+#    puts $pass
+#    user { 'testuser':
+#       ensure  => 'present',
+#       comment => 'TestUser,,,',
+#       groups  => ['staff', 'everyone'],
+#       home    => '/Users/testuser',
+#       shell   => '/bin/bash',
+#       password => $pass,
+#     }
+#  }
+#
+#  include associate-user
